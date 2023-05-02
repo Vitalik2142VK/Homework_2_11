04866cc60fba5b1110f0990c9bbe9307c4525f26
Vitalii <lennab@mail.ru>
2023-05-02 11:41:39 +0300

    First commit

---
+++ src/test/java/hw/skypro/Homework211/Homework211ApplicationTests.java
@@ -0,0 +1,13 @@
+package hw.skypro.Homework211;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework211ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
